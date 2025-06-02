4db6da417ab81e2e68ad6cd5270abb7fbc84cc1e
Tom Herrmann <tomherrmanndev@gmail.com>
2025-06-02 15:27:49 -0400

    update projector src

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
   return (
     <section className="relative top-0 w-full md:h-[500px] sm:h-[300px] h-[275px] overflow-hidden z-10">
       <Image
-        src="https://s7qtxjaxzhtgrxvy.public.blob.vercel-storage.com/projector_blue-2qGh6Js0PtJrtSc4yVgfY7HBmgDw2W.png"
+        src="https://s7qtxjaxzhtgrxvy.public.blob.vercel-storage.com/projectorblue_hero"
         alt="Hero Image"
         fill
         className="object-cover -z-10"
