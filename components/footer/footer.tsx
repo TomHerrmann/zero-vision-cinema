f5d37189a88347f9c8fef4d34b72fdd4300b4d93
Tom Herrmann <tomherrmanndev@gmail.com>
2025-06-05 20:02:43 -0400

    add bound + ig link

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -7,15 +7,15 @@ import {
   llcName,
 } from '@/app/contsants/constants';
 import { Separator } from '../ui/separator';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer>
       <Separator />
       <section className="w-full my-8 p-12 text-lg">
-        <div className="flex flex-wrap justify-between gap-8 px-6 py-8">
-          {/* Site Map */}
-          <div className="w-full md:w-[48%] flex flex-col text-center">
+        <div className="flex flex-wrap justify-between gap-8 px-3 py-4">
+          <div className="w-full md:w-[29%] flex flex-col text-center">
             <h5 className="text-2xl font-semibold leading-none tracking-tight pb-6">
               Site Map
             </h5>
@@ -31,16 +31,38 @@ const Footer = () => {
               ))}
             </div>
           </div>
-          {/* Contact */}
-          <div className="w-full md:w-[48%] flex flex-col text-center">
+          <div className="w-full md:w-[29%] flex flex-col text-center items-center">
             <h5 className="text-2xl font-semibold leading-none tracking-tight pb-6">
               Contact
             </h5>
             <div className="pb-4">
+              <div className="flex items-center mb-2">
+                <Link
+                  href="https://www.instagram.com/zerovisioncinema/"
+                  target="_blank"
+                  className="flex"
+                >
+                  <Image
+                    className="mx-2"
+                    height={24}
+                    width={24}
+                    alt="instagram social media icon"
+                    src="https://s7qtxjaxzhtgrxvy.public.blob.vercel-storage.com/Instagram_Glyph_Gradient-cytRhDzf7XqHFRst9K38AotJg5pZ4i-acvkd6WqNAueeDECSqXGXbojnbEaCP.svg"
+                  />
+                  Follow Us On Instagram
+                </Link>
+              </div>
               <strong>
-                <Link href={`mailto:${emailAddress}`}>{emailAddress}</Link>
+                <Link href={`mailto:${emailAddress}`} target="_blank">
+                  {emailAddress}
+                </Link>
               </strong>
             </div>
+          </div>
+          <div className="w-full md:w-[29%] flex flex-col text-center items-center">
+            <h5 className="text-2xl font-semibold leading-none tracking-tight pb-6">
+              Address
+            </h5>
             <address className="not-italic text-md">
               <strong>{llcName}</strong>
               <p>{addressLine1}</p>
