09b6561cbf1c5df0974f003403f993450ad9c795
Tom Herrmann <tomherrmanndev@gmail.com>
2025-06-16 23:34:44 -0400

    replace header query with cookies

--- app/(frontend)/api/attendees/route.ts
+++ app/(frontend)/api/attendees/route.ts
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function GET(req: NextRequest) {
   try {
-    const token = req.headers.get('payload-token');
+    const token = req.cookies.get('payload-token');
 
     if (!token) {
       console.error('Unauthorized');
