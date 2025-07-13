f677d070f4381faf2c67ed2f05d7f2ad4d2e1029
Tom Herrmann <tomherrmanndev@gmail.com>
2025-07-13 18:18:44 -0400

    oops

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -11,9 +11,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <EventsSection
-        events={[...events, ...events, ...events, ...events, ...events]}
-      />
+      <EventsSection events={events} />
       <About />
       <NewsletterSignup />
       <ContactSection />
