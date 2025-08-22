afe3e90689c7a31021bc1cc5c1ab2f236e879c6a
Tom Herrmann <tomherrmanndev@gmail.com>
2025-08-21 22:13:21 -0400

    'force-dynamic' to stop caching

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -5,6 +5,8 @@ import ContactSection from '@/components/contact-section/contact-section';
 import Hero from '@/components/hero/hero';
 import { getUpcomingEvents } from '@/utils/getEvents';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const events = await getUpcomingEvents();
 
