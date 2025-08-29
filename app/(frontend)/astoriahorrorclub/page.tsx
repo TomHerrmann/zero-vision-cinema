5b78a21d4ac004afda03973f83e5a1a9e59c7c24
Tom Herrmann <tomherrmanndev@gmail.com>
2025-08-29 11:30:05 -0400

    ahc page spacing

--- app/(frontend)/astoriahorrorclub/page.tsx
+++ app/(frontend)/astoriahorrorclub/page.tsx
@@ -11,8 +11,8 @@ const creepsterfont = Creepster({
 
 export default async function AstoriaHorrorClubPage() {
   return (
-    <main className="m-20">
-      <section className="flex justify-center">
+    <main className="m-8 mb-0 md:mx-12 md:mt-20">
+      <section className="flex w-full justify-center">
         <Image
           src={AHC_LOGO_PNG_URL}
           alt="astoria horror club logo"
@@ -22,7 +22,7 @@ export default async function AstoriaHorrorClubPage() {
       </section>
       <section
         id="about"
-        className="flex flex-col justify-center align-center rounded-none border-none p-12 w-full"
+        className="flex flex-col justify-center align-center rounded-none border-none p-2 md:p-12 w-full"
       >
         <div className="text-center">
           <h2
@@ -35,7 +35,7 @@ export default async function AstoriaHorrorClubPage() {
           </h2>
         </div>
         <div className="flex justify-center text-center">
-          <p className="md:w-3/4 text-xl md:px-8">
+          <p className="text-xl md:px-8">
             Astoria Horror Club builds community through a shared love of scary
             movies and books. AHC was started in 2021 with a reddit post to
             r/astoria with the goal of finding other horror fans in the
