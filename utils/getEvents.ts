36eba77bf3242d9ffc4af80d8fe381b7a1ae7615
Tom Herrmann <tomherrmanndev@gmail.com>
2026-02-21 14:45:25 -0500

    add static event for UP

--- utils/getEvents.ts
+++ utils/getEvents.ts
@@ -1,5 +1,6 @@
 import payloadConfig from '@/payload.config';
 import { getPayload } from 'payload';
+import type { Event, Location, Media } from '@/payload-types';
 
 const payload = await getPayload({ config: payloadConfig });
 
@@ -20,6 +21,60 @@ export const getUpcomingEvents = async () => {
     sort: ['datetime'],
     depth: 1,
   });
+
+  const image: Media = {
+    id: 0,
+    alt: '',
+    updatedAt: '2026-02-23 13:20:14.227+00',
+    createdAt: '2026-02-23 13:20:14.227+00',
+    url: 'https://s7qtxjaxzhtgrxvy.public.blob.vercel-storage.com/UP_5YR_FocalPoint_2.jpg',
+  };
+
+  const location: Location = {
+    id: 2,
+    name: 'Focal Point Beer Co',
+    address: '43-50 12th St',
+    city: 'Long Island City',
+    state: 'NY',
+    zip: 11101,
+    url: 'https://www.focalpoint.beer/',
+    updatedAt: '2025-06-01 17:20:14.193+00',
+    createdAt: '2025-06-01 17:20:14.227+00',
+  };
+
+  const staticDoc: Event = {
+    id: 10000,
+    name: 'Uncle Peckerhead | 5th Anniversary Screening',
+    description: {
+      root: {
+        type: '',
+        children: [],
+        direction: null,
+        format: '',
+        indent: 0,
+        version: 0,
+      },
+    },
+    image,
+    price: 15,
+    location,
+    paymentLink:
+      'https://www.eventbrite.com/e/uncle-peckerhead-5-year-anniversary-screening-at-focal-point-beer-co-tickets-1981499252605',
+    ticketLimit: 100,
+    ticketsSold: 0,
+    datetime: '2026-03-22 18:00:00.000+0000',
+    updatedAt: '2026-02-21 18:38:53.855+00',
+    createdAt: '2026-02-21 18:38:53.855+00',
+  };
+  console.log(
+    { staticDoc },
+    'Should show: ',
+    staticDoc.datetime > nowMinus30Minutes
+  );
+
+  if (new Date(staticDoc.datetime) > new Date(nowMinus30Minutes)) {
+    docs.push(staticDoc);
+  }
 
   return docs;
 };
