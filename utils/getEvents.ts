8cec1d31b539b46f15b5876d3b8eefef06b14f6b
Tom Herrmann <tomherrmanndev@gmail.com>
2026-07-28 11:23:45 -0400

    remove static docs

--- utils/getEvents.ts
+++ utils/getEvents.ts
@@ -25,60 +25,6 @@ export const getUpcomingEvents = async () => {
     depth: 1,
   });
 
-  const image: Media = {
-    id: 0,
-    alt: 'zvc anniverary show poster',
-    updatedAt: '2026-02-23 13:20:14.227+00',
-    createdAt: '2026-02-23 13:20:14.227+00',
-    url: 'https://s7qtxjaxzhtgrxvy.public.blob.vercel-storage.com/ZERO%20VISION%20CINEMA%201%20YEAR%20ANNIVERSARY%20SHOW.png',
-  };
-
-  const location: Location = {
-    id: 3,
-    name: 'SingleCut Beersmiths QNS',
-    address: '19-33 37th St',
-    city: 'Astoria',
-    state: 'NY',
-    zip: 11105,
-    url: 'https://www.singlecut.com/',
-    updatedAt: '2025-06-01 17:20:14.193+00',
-    createdAt: '2025-06-01 17:20:14.227+00',
-  };
-
-  const staticDoc: Event = {
-    id: 10000,
-    eventType: 'zvc',
-    name: 'ZVC 1 Year Anniverary Show',
-    imdbId: '',
-    description: {
-      root: {
-        type: '',
-        children: [],
-        direction: null,
-        format: '',
-        indent: 0,
-        version: 0,
-      },
-    },
-    image,
-    price: 0,
-    location,
-    paymentLink:
-      'https://www.eventbrite.com/e/uncle-peckerhead-5-year-anniversary-screening-at-focal-point-beer-co-tickets-1981499252605',
-    ticketLimit: 100,
-    ticketsSold: 0,
-    datetime: '2026-05-06 19:00:00.000+0000',
-    updatedAt: '2026-02-21 18:38:53.855+00',
-    createdAt: '2026-02-21 18:38:53.855+00',
-  };
-
-  if (new Date(staticDoc.datetime) > new Date(nowMinus30Minutes)) {
-    docs.push(staticDoc);
-    docs.sort(
-      (a, b) => new Date(a.datetime).getTime() - new Date(b.datetime).getTime()
-    );
-  }
-
   return docs;
 };
 
